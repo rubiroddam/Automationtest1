e27eda14f6bfbf307c185171a75349c04ba769f8
RoddamR <RoddamR@MELYN3193>
2018-09-14 10:39:22 +1000

    Add project files.

---
+++ Automationtest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Automationtest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
